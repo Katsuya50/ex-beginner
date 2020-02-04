189a42f18e159fa201e866d58769c4a2425c0234
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-04 16:30:31 +0900

    氏名あいまい検索用のrepositoryクラスの作成

---
+++ src/main/java/com/example/repository/MemberRepository.java
@@ -0,0 +1,39 @@
+package com.example.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.Member;
+
+@Repository
+public class MemberRepository {
+	
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	
+	private static final String TABLE_NAME = "members";
+	
+	private static final RowMapper<Member> MEMBER_ROW_MAPPER = (rs, i) -> {
+		Member member = new Member();
+		member.setId(rs.getInt("id"));
+		member.setName(rs.getString("name"));
+		member.setAge(rs.getInt("age"));
+		member.setDepId(rs.getInt("dep_id"));
+		return member;
+	};
+	
+	public List<Member> findByName(String name){
+		String sql = "SELECT * FROM " + TABLE_NAME
+				+ " WHERE name LIKE :name ORDER BY id";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("name", "%" + name + "%");
+		List<Member> memberList = template.query(sql, param, MEMBER_ROW_MAPPER);
+		return memberList;
+	}
+
+}
